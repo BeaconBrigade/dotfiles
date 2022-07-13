50b7b640c232dd1bbe28730af0e46a788294b832
BeaconBrigade <beaconbrigade@gmail.com>
2022-07-13 16:47:35 -0230

    Added Explore keymap, fixed keymap collision

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -31,3 +31,5 @@ keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
 
+-- Open up Explore
+keymap("n", "<leader>ex<cr>", ":Explore", opts)
