f6a59f82b91939f3673f86e5a345e4215ef7aac1
BeaconBrigade <beaconbrigade@gmail.com>
2022-07-14 10:47:46 -0230

    Added java, and comments

--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -11,29 +11,40 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
+   -- Json code
 	 if server.name == "jsonls" then
 	 	local jsonls_opts = require("user.lsp.settings.jsonls")
 	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 	 end
 
+   -- Lua code
 	 if server.name == "sumneko_lua" then
 	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
 	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	 end
 
+   -- Python code
 	 if server.name == "pyright" then
 	 	local pyright_opts = require("user.lsp.settings.pyright")
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	 end
 
+   -- Rust code
    if server.name == "rust_analyzer" then
     local rust_analyzer_opts = require("user.lsp.settings.rust_analyzer")
     opts = vim.tbl_deep_extend("force", rust_analyzer_opts, opts);
    end
 
+   -- Javascript / Ts code
    if server.name == "tsserver" then
     local tsserver_opts = require("user.lsp.settings.tsserver")
     opts = vim.tbl_deep_extend("force", tsserver_opts, opts);
+   end
+
+   -- Java code
+   if server.name == "jdtls" then
+    local jdtls_opts = require("user.lsp.settings.jdtls")
+    opts = vim.tbl_deep_extend("force", jdtls_opts, opts);
    end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
