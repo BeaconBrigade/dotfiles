83aeef0181ea55a1620fc38a6a99ba6d4092dc35
BeaconBrigade <beaconbrigade@gmail.com>
2022-07-15 12:16:19 -0230

    fmt and autopairs

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,107 +1,108 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 return packer.startup(function(use)
-    -- Packer
-    use 'wbthomason/packer.nvim'
-    
-    -- Completion
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-cmdline'
-    use 'saadparwaiz1/cmp_luasnip'
+	-- Packer
+	use("wbthomason/packer.nvim")
 
-    -- Snippet
-    use 'hrsh7th/vim-vsnip'
-    use 'hrsh7th/vim-vsnip-integ'
-    use 'L3MON4D3/LuaSnip' --snippet engine
-    use 'rafamadriz/friendly-snippets' -- a bunch of snippets to use
+	-- Completion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lsp-signature-help")
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-cmdline")
+	use("saadparwaiz1/cmp_luasnip")
+	use("windwp/nvim-autopairs") -- matching `{` with a `}` and so on
 
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
-    use 'j-hui/fidget.nvim'
-    use 'm-demare/hlargs.nvim'
+	-- Snippet
+	use("hrsh7th/vim-vsnip")
+	use("hrsh7th/vim-vsnip-integ")
+	use("L3MON4D3/LuaSnip") --snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-    -- Rust analyzer
-    use 'simrat39/rust-tools.nvim'
-    use 'weilbith/nvim-code-action-menu'
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
+	use("j-hui/fidget.nvim")
+	use("m-demare/hlargs.nvim")
 
-    -- Telescope
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-fzf-native.nvim'
-    use 'nvim-telescope/telescope-file-browser.nvim'
-    use 'nvim-lua/popup.nvim'
-    use 'nvim-lua/plenary.nvim'
-    use 'sharkdp/fd'
-    use 'BurntSushi/ripgrep'
-    use 'cljoly/telescope-repo.nvim'
+	-- Rust analyzer
+	use("simrat39/rust-tools.nvim")
+	use("weilbith/nvim-code-action-menu")
 
-    -- Colour Scheme
-    use 'RRethy/nvim-base16'
+	-- Telescope
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-fzf-native.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("sharkdp/fd")
+	use("BurntSushi/ripgrep")
+	use("cljoly/telescope-repo.nvim")
 
-    -- Treesitter
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-    }
-    use "p00f/nvim-ts-rainbow"
-    use "nvim-treesitter/playground"
+	-- Colour Scheme
+	use("RRethy/nvim-base16")
 
-    -- Lightline
-    use 'itchyny/lightline.vim'
-    use 'ryanoasis/vim-devicons'
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("p00f/nvim-ts-rainbow")
+	use("nvim-treesitter/playground")
 
-    -- Git 
-    use 'tpope/vim-fugitive'
+	-- Lightline
+	use("itchyny/lightline.vim")
+	use("ryanoasis/vim-devicons")
 
-    -- null-ls
-    use 'jose-elias-alvarez/null-ls.nvim'
+	-- Git
+	use("tpope/vim-fugitive")
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
+	-- null-ls
+	use("jose-elias-alvarez/null-ls.nvim")
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,25 +1,32 @@
-local configs = require("nvim-treesitter.configs")
-configs.setup {
-  ensure_installed = {
-      "c", 
-      "lua", 
-      "rust", 
-      "java", 
-      "javascript", 
-      "css", 
-      "html", 
-      "json", 
-      "markdown", 
-      "python", 
-      "vim" 
-    },
-  sync_install = false, 
-  auto_install = true,
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-}
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+configs.setup({
+	ensure_installed = {
+		"c",
+		"lua",
+		"rust",
+		"java",
+		"javascript",
+		"css",
+		"html",
+		"json",
+		"markdown",
+		"python",
+		"vim",
+	},
+	sync_install = false,
+	auto_install = true,
+	autopairs = {
+		enable = true,
+	},
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+})
