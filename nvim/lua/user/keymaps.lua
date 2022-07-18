31483dc7e74bd6e3879d13a2e715a864ba824666
BeaconBrigade <beaconbrigade@gmail.com>
2022-07-18 09:08:24 -0230

    Removed dumb keymap

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -31,8 +31,5 @@ keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
 
--- Open up Explore
-keymap("n", "<leader>ex<cr>", ":Explore", opts)
-
 -- Format document
 keymap("n", "<leader>fm<cr>", ":Format<cr>", opts)
