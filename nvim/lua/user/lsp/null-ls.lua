ad8d1f329cbb2ec50369e69767d3dbd8026e9d6a
BeaconBrigade <beaconbrigade@gmail.com>
2022-08-09 13:04:59 -0230

    fmt on save

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -10,6 +10,8 @@ local diagnostics = null_ls.builtins.diagnostics
 
 local code_actions = null_ls.builtins.code_actions
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	debug = false,
 	sources = {
@@ -24,4 +26,16 @@ null_ls.setup({
 		diagnostics.flake8,
 		code_actions.shellcheck,
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
