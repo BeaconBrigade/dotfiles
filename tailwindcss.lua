89bd93756ca00a5b05d5ff4bc061455a4fdf2396
BeaconBrigade <BeaconBrigade@gmail.com>
2022-08-31 11:44:06 -0230

    tailwind colours added, removed unused cssls config

---
+++ tailwindcss.lua
@@ -0,0 +1,14 @@
+local status_ok, tw_highlight = pcall(require, "tailwind-highlight")
+if not status_ok then
+	return
+end
+
+return {
+	on_attach = function(client, bufnr)
+		tw_highlight.setup(client, bufnr, {
+			single_column = false,
+			mode = "background",
+			debounce = 200,
+		})
+	end,
+}
