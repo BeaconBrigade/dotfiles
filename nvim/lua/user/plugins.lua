2fde71f02ce506e14c7905ac8b0add4ae610c5d9
BeaconBrigade <BeaconBrigade@gmail.com>
2022-08-31 11:00:08 -0230

    merge conflict

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
 	-- nice scrolling
 	use("karb94/neoscroll.nvim")
 
+	-- tailwind colours
+	use("princejoogie/tailwind-highlight.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
