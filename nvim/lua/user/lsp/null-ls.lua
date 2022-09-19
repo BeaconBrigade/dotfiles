cdb00dc205f14facbb354a3aaa84d985a7c5e55c
BeaconBrigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-09-19 15:33:19 -0230

    Commented out comment on save

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -10,7 +10,8 @@ local diagnostics = null_ls.builtins.diagnostics
 
 local code_actions = null_ls.builtins.code_actions
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- Format on save
+--[[ local augroup = vim.api.nvim_create_augroup("LspFormatting", {}) ]]
 
 null_ls.setup({
 	debug = false,
@@ -26,16 +27,17 @@ null_ls.setup({
 		diagnostics.flake8,
 		code_actions.shellcheck,
 	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+	-- Format on save
+	--[[ on_attach = function(client, bufnr) ]]
+	--[[ 	if client.supports_method("textDocument/formatting") then ]]
+	--[[ 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr }) ]]
+	--[[ 		vim.api.nvim_create_autocmd("BufWritePre", { ]]
+	--[[ 			group = augroup, ]]
+	--[[ 			buffer = bufnr, ]]
+	--[[ 			callback = function() ]]
+	--[[ 				vim.lsp.buf.format({ bufnr = bufnr }) ]]
+	--[[ 			end, ]]
+	--[[ 		}) ]]
+	--[[ 	end ]]
+	--[[ end, ]]
 })
