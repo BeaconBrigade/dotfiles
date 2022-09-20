05365eb57925d68b86e9e9f8bf632803e668c320
BeaconBrigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-09-19 23:07:50 -0230

    Better colouring for yank

--- nvim/lua/user/aucommands.lua
+++ nvim/lua/user/aucommands.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
   augroup highlight_yank
   autocmd!
-  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=100})
   augroup END
 ]])
